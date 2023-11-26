d6c128bae388144b929ccfb2963464a4c094f1ab
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2023-11-26 21:47:26 +0300

    0.0.2

--- Main/Main.js
+++ Main/Main.js
@@ -1,2 +1,3 @@
 import '../Api/Components/Menu/Menu.js';
 import '../Api/Components/HonorBoard/HonorBoard.js';
+import '../Api/Components/Footer/Footer.js';
--- Pages/About/Main/Main.js
+++ Pages/About/Main/Main.js
@@ -1,1 +1,2 @@
 import '../../../Api/Components/Menu/Menu.js';
+import '../../../Api/Components/Footer/Footer.js';
