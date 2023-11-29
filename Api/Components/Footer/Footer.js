8a1604679183bdc89844c0c0e1e9548e946b9771
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2023-11-29 18:17:18 +0300

    0.0.5

--- Api/Components/Footer/Footer.js
+++ Api/Components/Footer/Footer.js
@@ -6,14 +6,31 @@ export class Footer extends Component {
     static html = true;
     static url = import.meta.url;
 
+    _images = {
+        icon_vk: 'Vk.svg',
+        icon_youtube: 'Youtube.svg',
+    }
+
 
     static {
         this.init();
     }
 
     _init() {
+        this._set_img();
+        this._set_date();
+    }
+
+    _set_date() {
         let date_container = this._shadow.querySelector('.date');
         let date = new Date();
         date_container.textContent = '2023 — ' + date.getFullYear() + ' гг.';
+    }
+
+    _set_img() {
+        for (let img_name in this._images) {
+            let img = this._shadow.querySelector(`#${img_name}`);
+            img.setAttribute('src', `${import.meta.url}/../${this._images[img_name]}`);
+        }
     }
 }
